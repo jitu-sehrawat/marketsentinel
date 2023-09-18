a7b3c424ffc1c47e45138c9aa96487edb69ac8c4
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2023-09-18 11:45:19 +0530

    feat: company details and daily ohlc added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { JobsModule } from './jobs/jobs.module';
 import { MongooseModelsModule } from './schemas/mongoose-models.module';
+import { CompaniesModule } from './companies/company.module';
+import { QuotesModule } from './quotes/quote.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { MongooseModelsModule } from './schemas/mongoose-models.module';
     MongooseModelsModule,
     UsersModule,
     JobsModule,
+    CompaniesModule,
+    QuotesModule,
   ],
   controllers: [],
 })
--- src/schemas/mongoose-models.module.ts
+++ src/schemas/mongoose-models.module.ts
@@ -2,10 +2,14 @@ import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { USER_MODEL, UserSchema } from './user/user.schema';
 import { JOB_MODEL, JobSchema } from './job/job.schema';
+import { COMPANY_MODEL, CompanySchema } from './company';
+import { QUOTE_MODEL, QuoteSchema } from './quote';
 
 const MODELS = [
   { name: USER_MODEL, schema: UserSchema },
   { name: JOB_MODEL, schema: JobSchema },
+  { name: COMPANY_MODEL, schema: CompanySchema },
+  { name: QUOTE_MODEL, schema: QuoteSchema },
 ];
 
 @Global()
